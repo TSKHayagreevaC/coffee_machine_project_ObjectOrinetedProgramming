d1dd2f8f92ed073ad2166a5cb12cf342afd6375d
THIRUMALA SRIKANTH HAYAGREEVA CHARYA <69465021+TSKHayagreevaC@users.noreply.github.com>
2021-02-17 15:42:23 +0530

    commit with the coffee machine project using object oriented programming

---
+++ main.py
@@ -0,0 +1,52 @@
+# import another_module
+# print(another_module.another_variable)
+
+# from turtle import Turtle, Screen
+# timmy = Turtle()
+# print(timmy)
+# timmy.shape("turtle")
+# timmy.color("coral")
+# timmy.forward(100)
+#
+# my_screen = Screen()
+# print(my_screen.canvheight)
+# my_screen.exitonclick()
+
+# from prettytable import PrettyTable
+# table = PrettyTable()
+# table.add_column("Pokemon Name", ["Pikachu", "Squire", "Charmane"])
+# table.add_column("Type", ["Electric", "Water", "Fire"])
+#
+# table.align = "l"
+#
+# print(table)
+
+# A Coffee Machine Project...
+
+from menu import Menu, MenuItem
+from coffee_maker import CoffeeMaker
+from money_machine import MoneyMachine
+
+money_machine = MoneyMachine()
+coffee_maker = CoffeeMaker()
+menu = Menu()
+is_on = True
+
+coffee_maker.report()
+money_machine.report()
+
+while is_on:
+    options = menu.get_items()
+    choice = input(f"What Would You Like? ({options}): ")
+    if choice == "off":
+        is_on = False
+    elif choice == "report":
+        coffee_maker.report()
+        money_machine.report()
+    else:
+        drink = menu.find_drink(choice)
+        is_enough_ingredients = coffee_maker.is_resource_sufficient(drink)
+        is_payment_successful = money_machine.make_payment(drink.cost)
+        if coffee_maker.is_resource_sufficient(drink) and money_machine.make_payment(drink.cost):
+            coffee_maker.make_coffee(drink)
+
